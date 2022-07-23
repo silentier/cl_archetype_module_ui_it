bb348df7bf1fa052d3ae5fd995973fb0aae9534f
francisco v <administrator@silentier.com>
2022-07-22 23:17:20 -0400

    v.1.0.0

---
+++ src/main/resources/archetype-resources/ui-rest/src/main/java/Rest.java
@@ -0,0 +1,13 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class Rest {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Rest.class, args);
+    }
+
+}
+
---
+++ src/main/resources/archetype-resources/ui-service/src/main/java/service.java
@@ -0,0 +1,14 @@
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+
+import javax.annotation.PostConstruct;
+
+@Controller
+@Slf4j
+public class service {
+
+    @PostConstruct
+    public void init() {
+        log.info("@@ Controller loaded: "+this.getClass().getCanonicalName());
+    }
+}
