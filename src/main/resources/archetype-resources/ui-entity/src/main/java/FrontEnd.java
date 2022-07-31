e27cc26d4a43f915375bbfd40a4a0f4ae7281310
francisco v <administrator@silentier.com>
2022-07-31 11:14:57 -0400

    v0.0.3

---
+++ src/main/resources/archetype-resources/ui-entity/src/main/java/FrontEnd.java
@@ -0,0 +1,16 @@
+package ${package};
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+
+import javax.annotation.PostConstruct;
+
+@Controller
+@Slf4j
+public class Entity {
+
+    @PostConstruct
+    public void init() {
+        log.info("@@ Controller loaded: "+this.getClass().getCanonicalName());
+    }
+}
